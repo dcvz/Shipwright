b873ee7af69dc36f2a92be6b0f9907a892573862
aMannus <mannusmenting@gmail.com>
2022-09-26 23:06:44 +0200

    Comment

--- soh/soh/Enhancements/debugconsole.cpp
+++ soh/soh/Enhancements/debugconsole.cpp
@@ -672,6 +672,7 @@ static bool PacifistHandler(std::shared_ptr<Ship::Console> Console, const std::v
 
     try {
         pacifistMode = std::stoi(args[1], nullptr, 10) == 0 ? 0 : 1;
+        // Force interface to update to make the buttons transparent
         gSaveContext.unk_13E8 = 50;
         Interface_Update(gGlobalCtx);
         return CMD_SUCCESS;
